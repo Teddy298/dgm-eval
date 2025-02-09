e3f7ce4a87221f728cc6a7c1b1ad2784b26abd91
Marcin <kadziolka.marcin@outlook.com>
2025-02-09 19:45:18 +0100

    Add full script

--- dgm_eval/get_representations.py
+++ dgm_eval/get_representations.py
@@ -26,6 +26,87 @@ from pathlib import Path
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
+TEST_PATHS = ['/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ReACGAN/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ReACGAN/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ReACGAN/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ReACGAN/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ReACGAN/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ReACGAN/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-WGAN-GP/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-WGAN-GP/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-WGAN-GP/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-WGAN-GP/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-WGAN-GP/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-WGAN-GP/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-RESFLOW/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-RESFLOW/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-RESFLOW/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-RESFLOW/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-RESFLOW/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-RESFLOW/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-BigGAN-Deep/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-BigGAN-Deep/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-BigGAN-Deep/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-BigGAN-Deep/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-BigGAN-Deep/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-BigGAN-Deep/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LSGM-ODE/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LSGM-ODE/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LSGM-ODE/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LSGM-ODE/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LSGM-ODE/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LSGM-ODE/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-NVAE/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-NVAE/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-NVAE/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-NVAE/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-NVAE/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-NVAE/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-MHGAN/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-MHGAN/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-MHGAN/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-MHGAN/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-MHGAN/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-MHGAN/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LOGAN/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LOGAN/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LOGAN/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LOGAN/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LOGAN/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-LOGAN/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ACGAN-Mod/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ACGAN-Mod/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ACGAN-Mod/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ACGAN-Mod/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ACGAN-Mod/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-ACGAN-Mod/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN-XL/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN-XL/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN-XL/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN-XL/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN-XL/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN-XL/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-PFGMPP/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-PFGMPP/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-PFGMPP/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-PFGMPP/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-PFGMPP/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-PFGMPP/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-iDDPM-DDIM/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-iDDPM-DDIM/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-iDDPM-DDIM/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-iDDPM-DDIM/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-iDDPM-DDIM/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-iDDPM-DDIM/interpolation/80%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN2-ada/interpolation/20%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN2-ada/interpolation/0%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN2-ada/interpolation/60%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN2-ada/interpolation/100%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN2-ada/interpolation/40%train',
+              '/shared/sets/datasets/CIFAR10-dgm_eval/CIFAR10-StyleGAN2-ada/interpolation/80%train']
+
+
+
 parser.add_argument(
     "--model",
     type=str,
@@ -69,37 +150,36 @@ parser.set_defaults(load=True)
 parser.add_argument("--seed", type=int, default=13579, help="Random seed")
 
 
-def get_relevant_paths(main_folder_path, key_folder="interpolation"):
-    return [x[0] for x in os.walk(main_folder_path) if key_folder in x[0]]
 
+
+def get_relevant_paths(main_folder_path, key_value="%"):
+    return [x[0] for x in os.walk(main_folder_path) if key_value in x[0]]
+
+def create_output_paths(main_folder_path, model, paths):
+    main_folder_split = main_folder_path.split('/')
+    main_folder_name = main_folder_split[-1]
+    reps_folder = main_folder_name + "_reps"
+    outputs_paths = []
+    for path in paths:
+        splitted_path = path.split("/")
+        pos = splitted_path.index(main_folder_name)
+        subfolder_struct = splitted_path[pos+1:]
+        output_path = main_folder_split[:-1]
+        output_path.extend([reps_folder, model])
+        output_path.extend(subfolder_struct)
+        outputs_paths.append("/".join(output_path))
+    return outputs_paths
 
 def main():
-    output_dir = None
+    print("Starting man in get_representation")
     args = parser.parse_args()
 
-    main_folder_path = Path(args.folder_path)
-
+    main_folder_path = args.folder_path
+    print(f"{main_folder_path=}")
     relevant_paths = get_relevant_paths(main_folder_path)
-
-
-    print(relevant_paths)
-    return
-
-    new_folder = f"{main_folder_path}_reps"
-
-
-    # wjezdza sciezka do glownego folderu
-    # tam mam te wszystkie GANY
-    # wchodzac do GANA spodziewam sie 0, 1, batches i interpolations
-    # wylistowac foldery interpolations
-    # idac po kazdym folderze przerobic representacje
-    # i zapisac do odpowiednika tylko ze, datasets/<model>/<GAN>/<interpolations><X%train>
-    # na poczatku skrypt do listowania tych folderow ktore chce
-    parent_folder = main_folder_path.parent.absolute()
-
-
-
-    os.makedirs("path/to/directory", exist_ok=True)
+    print(f"{relevant_paths=}")
+    output_paths = create_output_paths(main_folder_path, args.model, relevant_paths)
+    print(f"{output_paths=}")
 
     device, num_workers = get_device_and_num_workers(args.device, args.num_workers)
 
@@ -120,16 +200,23 @@ def main():
         depth=args.depth,
     )
 
-    dataloader = get_dataloader_from_path(
-        args.folder_path, model.transform, num_workers, args
-    )
+    for img_path, output_path in zip(relevant_paths, output_paths):
+        dataloader = get_dataloader_from_path(
+            img_path, model.transform, num_workers, args
+        )
 
-    representations = compute_representations(dataloader, model, device, args)
+        representations = compute_representations(dataloader, model, device, args)
 
 
+        output_path = os.path.join(output_path, "repr.npz")
+        print(f"Saving representations to {output_path}\n", file=sys.stderr)
+        hyperparams = vars(dataloader).copy()  # Remove keys that can't be pickled
+        hyperparams.pop("transform")
+        hyperparams.pop("data_loader")
+        hyperparams.pop("data_set")
 
-    print(f"Saving representations to {output_dir}\n", file=sys.stderr)
-    save_outputs(args.output_dir, representations, args.model, None, dataloader)
+        np.savez(output_path, model=model, reps=representations, hparams=hyperparams)
+
 
 
 
